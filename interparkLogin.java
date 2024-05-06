36185750250e54c398e029144701deee658a18b7
czyier1212 <z1654230@naver.com>
2024-05-06 16:28:23 +0900

    update

--- interparkLogin.java
+++ interparkLogin.java
@@ -1,65 +1,39 @@
-const https = require('https'); 
-const readline = require('readline'); 
-const querystring = require('querystring'); 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-function interpark_login(userId, userPwd) {
-    const postData = querystring.stringify({
-        "frUID": userId, 
-        "frPWD": userPwd 
-    });
+public class InterparkLogin {
+    public static void main(String[] args) throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        String userId = "your_user_id";
+        String userPwd = "your_password";
 
-    // 요청 옵션
-    const options = {
-        hostname: 'ticket.interpark.com', 
-        port: 443, 
-        path: '/gate/TPLoginCheck_Return.asp', 
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded', 
-            'Content-Length': Buffer.byteLength(postData) 
-        }
-    };
+        String postData = "frUID=" + URLEncoder.encode(userId, "UTF-8") +
+                          "&frPWD=" + URLEncoder.encode(userPwd, "UTF-8");
 
-    // 1. HTTP 요청 
-    const req = https.request(options, (res) => {
-        let data = ''; 
+        HttpPost request = new HttpPost("https://ticket.interpark.com/gate/TPLoginCheck_Return.asp");
+        request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.setEntity(new StringEntity(postData));
 
-        // 3. 응답 수신
-        res.on('data', (chunk) => {
-            data += chunk; 
-        });
+        HttpResponse response = httpClient.execute(request);
+        HttpEntity entity = response.getEntity();
 
-        // 응답 처리
-        res.on('end', () => {
-		console.log('커밋테스트 111');
-            console.log('Response:', data); 
+        if (entity != null) {
+            String result = EntityUtils.toString(entity);
+            System.out.println("Response: " + result);
 
-            if (data === '0') {
-                console.log(`안녕하세요. [${userId}]님!`); 
+            if ("0".equals(result)) {
+                System.out.println("안녕하세요. [" + userId + "]님!");
             } else {
-                console.error('로그인에 실패하였습니다.ㅠㅠ'); 
+                System.err.println("로그인에 실패하였습니다.ㅠㅠ");
             }
-        });
-    });
-
-    req.on('error', (e) => {
-        console.error('서버 요청 실패(login error)', e); 
-    });
-
-    // 2. 요청 데이터 전송
-    req.write(postData); 
-    req.end(); 
+        }
+    }
 }
-
-
-const rl = readline.createInterface({
-    input: process.stdin, 
-    output: process.stdout 
-});
-
-rl.question('id: ', (userId) => {
-    rl.question('pwd: ', (userPwd) => {
-        interpark_login(userId, userPwd);
-        rl.close(); 
-    });
-});
